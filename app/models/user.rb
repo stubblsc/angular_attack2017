dfa6cf65623100fbb7471cc7f44391543b126df2
Scott Stubblebine <scott.c.stubblebine@gmail.com>
2017-04-22 02:16:08 -0700

    fixed relationships

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,7 @@ class User < ActiveRecord::Base
           :recoverable, :rememberable, :trackable, :validatable,
           :confirmable, :omniauthable
   include DeviseTokenAuth::Concerns::User
+
+  has_many :songs, inverse_of: :user
+  has_many :comments, inverse_of: :user
 end
