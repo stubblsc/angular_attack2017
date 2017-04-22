2e1f057b09fec311864d7b0251cfe1225a6da81e
Scott Stubblebine <scott.c.stubblebine@gmail.com>
2017-04-22 01:20:36 -0700

    added initial song stuff

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170422071919) do
+ActiveRecord::Schema.define(version: 20170422081947) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "songs", force: :cascade do |t|
+    t.integer  "user_id"
+    t.string   "name"
+    t.binary   "payload"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_songs_on_user_id", using: :btree
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "provider",               default: "email", null: false
@@ -44,4 +53,5 @@ ActiveRecord::Schema.define(version: 20170422071919) do
     t.index ["uid", "provider"], name: "index_users_on_uid_and_provider", unique: true, using: :btree
   end
 
+  add_foreign_key "songs", "users"
 end
